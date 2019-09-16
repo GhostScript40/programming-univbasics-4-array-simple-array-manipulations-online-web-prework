fd581f608e125a1de563af5f429dcc7e835af66d
Tariq Bailey <tariqbailey627@gmail.com>
2019-09-16 03:08:03 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push(colors_in_the_rainbow, next_color)
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+next_color = "violet"
+colors_in_the_rainbow.push(string)
+end
+
+def using_unshift(array, string)
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+new_neighborhood = "Staten Island"
+new_neighborhood.unshift(array)
+end
+
+def using_pop(array)
+array.pop
+end
+
+def pop_with_args(array)
+array.pop(2)
+end
+
+def using_shift(array)
+array.shift
+end
+
+def shift_with_args(array)
+  array.shit(2)
+end
+  
+using_contact  
